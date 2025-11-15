6ff591d0645108ca47cf98cbf3064cdf1a5f184c
Artem Topolsky <almost210690@gmail.com>
2025-11-15 03:34:45 +0200

    fix userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,13 +1,12 @@
 import { Router } from 'express';
 import { celebrate } from 'celebrate';
 import { getCurrentUser, updateUserProfile } from '../controllers/userController.js';
-import { upload } from '../middleware/multer.js';
 import { authenticate } from '../middleware/authenticate.js';
 import { updateUserSchema } from '../validations/userValidation.js';
 
 const router = Router();
 
-router.patch('/users/me', authenticate, upload.single('avatar'), celebrate(updateUserSchema), updateUserProfile);
+router.patch('/users/me', authenticate, celebrate(updateUserSchema), updateUserProfile);
 router.get('/users/me', authenticate, getCurrentUser);
 
 export default router;
