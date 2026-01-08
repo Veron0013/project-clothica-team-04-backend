1eac9474b89f483069e5859aedde183043609aa2
Mr.Igor <iv_mirsoft@ukr.net>
2026-01-08 07:40:26 +0200

    fixed errorhandler auth controller syntax

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,6 +1,7 @@
 // src/middleware/errorHandler.js
 import { HttpError } from 'http-errors';
 
+// eslint-disable-next-line no-unused-vars
 export const errorHandler = (error, req, res, next) => {
 	if (error instanceof HttpError) {
 		return res.status(error.status).json({
@@ -8,34 +9,34 @@ export const errorHandler = (error, req, res, next) => {
 		});
 	}
 
-	console.error(error);
+	//console.error(error);
 
 	if (error.name === 'JsonWebTokenError') {
-    return res.status(401).json({
-      message: 'Токен недійсний. Авторизуйтеся повторно.',
-    });
+		return res.status(401).json({
+			message: 'Токен недійсний. Авторизуйтеся повторно.',
+		});
 	}
-	
+
 	if (error.name === 'TokenExpiredError') {
-    return res.status(401).json({
-      message: 'Термін дії токена минув. Авторизуйтеся повторно.',
-    });
+		return res.status(401).json({
+			message: 'Термін дії токена минув. Авторизуйтеся повторно.',
+		});
 	}
-	
+
 	if (error.name === 'CastError') {
-    return res.status(400).json({
-      message: 'Некоректний ідентифікатор ресурсу.',
-    });
+		return res.status(400).json({
+			message: 'Некоректний ідентифікатор ресурсу.',
+		});
 	}
-	
+
 	if (error.name === 'ValidationError') {
-    return res.status(400).json({
-      message: 'Некоректні дані. Перевірте правильність введення.',
-    });
+		return res.status(400).json({
+			message: 'Некоректні дані. Перевірте правильність введення.',
+		});
 	}
-		
+
 	return res.status(500).json({
-    message: 'Сталася внутрішня помилка сервера. Спробуйте пізніше.',
+		message: 'Сталася внутрішня помилка сервера. Спробуйте пізніше.',
 	});
-	
+
 };
